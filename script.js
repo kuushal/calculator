b60d5e97e447a4a8842e31eea37ad4b6d79a7a2c
Kushal <27188278+kuushal@users.noreply.github.com>
2023-02-27 11:57:42 +0530

    calculations are working with decimals

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 const clearButton = document.querySelector('.clear');
 const result = document.querySelector('.result');
-const operatorAndOperands = document.querySelectorAll('li');
 const calculateResult = document.querySelector('.equals');
+const allOpertors = document.querySelectorAll('.operator');
+const operands = document.querySelectorAll('.operand');
 
 let input = '';
 
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+
 // eventListeners
 clearButton.addEventListener('click', clearResult);
-operatorAndOperands.forEach(operate => operate.addEventListener('click', getInput));
+allOpertors.forEach(eachOperator => eachOperator.addEventListener('click', getOperator));
+operands.forEach(operand => operand.addEventListener('click', getFirstInput));
 calculateResult.addEventListener('click', calculate);
 
+
+
 function calculate() {
-    let first = +input.charAt(0);
-    let second = +input.charAt(2);
-    let oper = input.charAt(1);
-
-    input = operate(oper, first, second);
+    secondNumber = +input;
+    input = operate(operator, firstNumber, secondNumber);
     result.textContent = input;
-
 }
 
 
@@ -26,9 +30,16 @@ function clearResult() {
     result.textContent = '0';
 }
 
-function getInput(e) {
+function getFirstInput(e) {
     input += e.target.innerHTML;
+    console.log(input);
     result.textContent = input;
+}
+
+function getOperator(e) {
+    firstNumber = +input;
+    operator = e.target.innerHTML;
+    clearResult();
 }
 
 function add(first, second) {
@@ -44,13 +55,16 @@ function multiply(first, second) {
 }
 
 function divide(first, second) {
+    if (second == 0) {
+        return 'lmao';
+    }
     return +first / +second;
 }
 
 function operate(operator, first, second) {
     if (operator == "+") {
         return add(first, second);
-    } else if (operator == "-") {
+    } else if (operator == "−") {
         return subtract(first, second);
     } else if (operator == '×') {
         return multiply(first, second);
